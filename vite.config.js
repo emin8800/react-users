67d5c37d83c07edf55adc947f8e59d067a8bbda2
Emin <99510258+emin8800@users.noreply.github.com>
2024-08-23 00:29:47 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // veya başka bir port numarası
+    host: '0.0.0.0' // Tüm ağ arayüzleri üzerinden erişilebilir
+  }
+});
